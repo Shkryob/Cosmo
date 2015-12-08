d31439107a7af7f04ec522bbfeb0dfe899f0c2dc
Kostya Shkryob <shkryob@ya.ru>
2015-12-08 21:04:49 +0200

    Don't minify scripts in developer mode

--- core/app/initialize.php
+++ core/app/initialize.php
@@ -14,6 +14,7 @@ $minifyScripts = 'min/?f=';
 $minifyCSS = 'min/?f=';
 $scripts = '';
 $CSS = '';
+$coreScripts = array();
 
 // Log user in if they have a cookie
 if(isset($_COOKIE['usersID']) && $_COOKIE['usersID'] && $_COOKIE['token'])
@@ -33,89 +34,96 @@ if(isset($_COOKIE['usersID']) && $_COOKIE['usersID'] && $_COOKIE['token'])
         $token = $_COOKIE['token'];
         //setcookie('token', $token, time()+60*60*24*90); // Set cookie to expire in 90 days
 
-        $minifyScripts .= FOLDER."core/js/3rd-party/angular-file-upload-shim.min.js,"; // Breaks IE9, so only load it for admins
+        $coreScripts[] = FOLDER."core/js/3rd-party/angular-file-upload-shim.min.js"; // Breaks IE9, so only load it for admins
     }
 }
 
 // Load official Angular files
-$minifyScripts .= FOLDER."core/js/angular/angular.min.js,";
-$minifyScripts .= FOLDER."core/js/angular/angular-animate.min.js,";
-$minifyScripts .= FOLDER."core/js/angular/angular-touch.min.js,";
-$minifyScripts .= FOLDER."core/js/angular/angular-route.min.js,";
-$minifyScripts .= FOLDER."core/js/angular/angular-resource.min.js,";
+$coreScripts[] = FOLDER."core/js/angular/angular.min.js";
+$coreScripts[] = FOLDER."core/js/angular/angular-animate.min.js";
+$coreScripts[] = FOLDER."core/js/angular/angular-touch.min.js";
+$coreScripts[] = FOLDER."core/js/angular/angular-route.min.js";
+$coreScripts[] = FOLDER."core/js/angular/angular-resource.min.js";
 
 // Load the Cosmo module
-$minifyScripts .= FOLDER."core/js/cosmo.js,";
+$coreScripts[] = FOLDER."core/js/cosmo.js";
 
 // Load the Cosmo admin panel files
-$minifyScripts .= FOLDER."core/js/admin-panel/admin-panel.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/block.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/content-list.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/files.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/login-registration.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/menu.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/module.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/page.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/profile.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/reset-password.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/revisions.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/settings.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/theme.js,";
-$minifyScripts .= FOLDER."core/js/admin-panel/users.js,";
+$coreScripts[] = FOLDER."core/js/admin-panel/admin-panel.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/block.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/content-list.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/files.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/login-registration.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/menu.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/module.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/page.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/profile.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/reset-password.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/revisions.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/settings.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/theme.js";
+$coreScripts[] = FOLDER."core/js/admin-panel/users.js";
 
 // Load the directive files
-$minifyScripts .= FOLDER."core/js/directives/csAnchor.js,";
-$minifyScripts .= FOLDER."core/js/directives/csAudio.js,";
-$minifyScripts .= FOLDER."core/js/directives/csBgImage.js,";
-$minifyScripts .= FOLDER."core/js/directives/csBlock.js,";
-$minifyScripts .= FOLDER."core/js/directives/csContent.js,";
-$minifyScripts .= FOLDER."core/js/directives/csGallery.js,";
-$minifyScripts .= FOLDER."core/js/directives/csImage.js,";
-$minifyScripts .= FOLDER."core/js/directives/csLogo.js,";
-$minifyScripts .= FOLDER."core/js/directives/csMenu.js,";
-$minifyScripts .= FOLDER."core/js/directives/csMovie.js,";
-$minifyScripts .= FOLDER."core/js/directives/csNotification.js,";
-$minifyScripts .= FOLDER."core/js/directives/csResult.js,";
-$minifyScripts .= FOLDER."core/js/directives/csSearch.js,";
-$minifyScripts .= FOLDER."core/js/directives/csSlogan.js,";
-$minifyScripts .= FOLDER."core/js/directives/csTable.js,";
-$minifyScripts .= FOLDER."core/js/directives/csTitle.js,";
-$minifyScripts .= FOLDER."core/js/directives/csWysiwyg.js,";
-$minifyScripts .= FOLDER."core/js/directives/fluidvids.js,";
+$coreScripts[] = FOLDER."core/js/directives/csAnchor.js";
+$coreScripts[] = FOLDER."core/js/directives/csAudio.js";
+$coreScripts[] = FOLDER."core/js/directives/csBgImage.js";
+$coreScripts[] = FOLDER."core/js/directives/csBlock.js";
+$coreScripts[] = FOLDER."core/js/directives/csContent.js";
+$coreScripts[] = FOLDER."core/js/directives/csGallery.js";
+$coreScripts[] = FOLDER."core/js/directives/csImage.js";
+$coreScripts[] = FOLDER."core/js/directives/csLogo.js";
+$coreScripts[] = FOLDER."core/js/directives/csMenu.js";
+$coreScripts[] = FOLDER."core/js/directives/csMovie.js";
+$coreScripts[] = FOLDER."core/js/directives/csNotification.js";
+$coreScripts[] = FOLDER."core/js/directives/csResult.js";
+$coreScripts[] = FOLDER."core/js/directives/csSearch.js";
+$coreScripts[] = FOLDER."core/js/directives/csSlogan.js";
+$coreScripts[] = FOLDER."core/js/directives/csTable.js";
+$coreScripts[] = FOLDER."core/js/directives/csTitle.js";
+$coreScripts[] = FOLDER."core/js/directives/csWysiwyg.js";
+$coreScripts[] = FOLDER."core/js/directives/fluidvids.js";
 
 // Load the factory files
-$minifyScripts .= FOLDER."core/js/factories/hooks.js,";
-$minifyScripts .= FOLDER."core/js/factories/page.js,";
-$minifyScripts .= FOLDER."core/js/factories/plaintext.js,";
-$minifyScripts .= FOLDER."core/js/factories/responsive.js,";
-$minifyScripts .= FOLDER."core/js/factories/rest.js,";
-$minifyScripts .= FOLDER."core/js/factories/users.js,";
+$coreScripts[] = FOLDER."core/js/factories/hooks.js";
+$coreScripts[] = FOLDER."core/js/factories/page.js";
+$coreScripts[] = FOLDER."core/js/factories/plaintext.js";
+$coreScripts[] = FOLDER."core/js/factories/responsive.js";
+$coreScripts[] = FOLDER."core/js/factories/rest.js";
+$coreScripts[] = FOLDER."core/js/factories/users.js";
 
 // Load the filter files
-$minifyScripts .= FOLDER."core/js/filters/image-size.js,";
-$minifyScripts .= FOLDER."core/js/filters/theme-files.js,";
-$minifyScripts .= FOLDER."core/js/filters/timestamp.js,";
-$minifyScripts .= FOLDER."core/js/filters/titlecase.js,";
+$coreScripts[] = FOLDER."core/js/filters/image-size.js";
+$coreScripts[] = FOLDER."core/js/filters/theme-files.js";
+$coreScripts[] = FOLDER."core/js/filters/timestamp.js";
+$coreScripts[] = FOLDER."core/js/filters/titlecase.js";
 
 // Load the front-end controller files
-$minifyScripts .= FOLDER."core/js/front-end/comments.js,";
-$minifyScripts .= FOLDER."core/js/front-end/content.js,";
-$minifyScripts .= FOLDER."core/js/front-end/html.js,";
-$minifyScripts .= FOLDER."core/js/front-end/link.js,";
-$minifyScripts .= FOLDER."core/js/front-end/url.js,";
-$minifyScripts .= FOLDER."core/js/front-end/wysiwyg.js,";
+$coreScripts[] = FOLDER."core/js/front-end/comments.js";
+$coreScripts[] = FOLDER."core/js/front-end/content.js";
+$coreScripts[] = FOLDER."core/js/front-end/html.js";
+$coreScripts[] = FOLDER."core/js/front-end/link.js";
+$coreScripts[] = FOLDER."core/js/front-end/url.js";
+$coreScripts[] = FOLDER."core/js/front-end/wysiwyg.js";
 
 // 3rd party scripts
-$minifyScripts .= FOLDER."core/js/3rd-party/angular-file-upload.min.js,";
-$minifyScripts .= FOLDER."core/js/3rd-party/angular-translate.min.js,";
-$minifyScripts .= FOLDER."core/js/3rd-party/angular-translate-loader-static-files.min.js,";
-// $minifyScripts .= FOLDER."core/js/3rd-party/angular-translate-storage-cookie.min.js,";
-$minifyScripts .= FOLDER."core/js/3rd-party/diff_match_patch.js,";
-$minifyScripts .= FOLDER."core/js/3rd-party/angular-ui-tree.min.js";
+$coreScripts[] = FOLDER."core/js/3rd-party/angular-file-upload.min.js";
+$coreScripts[] = FOLDER."core/js/3rd-party/angular-translate.min.js";
+$coreScripts[] = FOLDER."core/js/3rd-party/angular-translate-loader-static-files.min.js";
+// $coreScripts[] = FOLDER."core/js/3rd-party/angular-translate-storage-cookie.min.js";
+$coreScripts[] = FOLDER."core/js/3rd-party/diff_match_patch.js";
+$coreScripts[] = FOLDER."core/js/3rd-party/angular-ui-tree.min.js";
 
 
 $minifyCSS .= FOLDER."core/css/cosmo-default-style.minify.css";
 
+if ($developerMode) {
+    foreach ($coreScripts as $script) {
+        $scripts .= "<script src='" . $script . "'></script>\n\t";
+    }
+} else {
+    $minifyScripts .= implode(',', $coreScripts);
+}
 
 // Load menus
 $menus = $Cosmo->menusRead();
